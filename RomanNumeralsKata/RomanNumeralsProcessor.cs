720a9f888d4a98b6eb9e39ad225c172a11eef880
Salim Rahmani <SalimRahmani@outlook.com>
2018-03-30 11:34:18 +0200

    added the first case when the input is 1, it should return I

---
+++ RomanNumeralsKata/RomanNumeralsProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata
+{
+    public class RomanNumeralsProcessor
+    {
+        public string Convert(int input)
+        {
+            return "I";
+        }
+    }
+}
---
+++ RomanNumeralsKataTest/RomanNumeralsTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using RomanNumeralsKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKataTest
+{
+    public class RomanNumeralsTest
+    {
+        [TestCase(1, "I")]
+        public void should_return_I_when_input_is_1(int input, string expected)
+        {
+            // arrange
+            RomanNumeralsProcessor romanNumeralsProcessor = new RomanNumeralsProcessor();
+
+            // act
+            string result = romanNumeralsProcessor.Convert(input);
+
+            // assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
